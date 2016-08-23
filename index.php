9041e2ea6f7677a0de3f09c93033670312f8ef37
leandrovidal92 <leandrovidal92@gmail.com>
2016-08-23 20:14:46 -0300

    Ejercicio 9

--- index.php
+++ index.php
@@ -39,6 +39,40 @@
 					echo "<br>";
 					echo $key . " => " . $value;
 				}
+		
+		echo "<br>";
+		echo"-------------------------------------------------------------------------";
+		echo "<br>";
+		echo "Ejercicio 9";
+		echo "<br>";
+		echo "<br>";
+
+			$lapicera= array('color'=>'Celeste', 'marca'=>'Pencil','trazo'=>'fino','precio'=>10);
+			$lapicera1= array('color'=>'Blanca', 'marca'=>'Faber','trazo'=>'Gruesa','precio'=>12);
+			$lapicera2= array('color'=>'Azul', 'marca'=>'otro','trazo'=>'medio','precio'=>9);
+
+			
+
+			
+			foreach ($lapicera as $key => $value) 
+			{
+				echo $key." de la lapicera: ".$value;
+				echo "<br>";
+			}
+					echo "<br>";
+			
+				foreach ($lapicera1 as $key => $value) 
+			{
+				echo $key." de la lapicera: ".$value;
+				echo "<br>";
+			}
+					echo "<br>";
+			
+				foreach ($lapicera2 as $key => $value) 
+			{
+				echo $key." de la lapicera: ".$value;
+				echo "<br>";
+			}
 		?>
 
 </body>
